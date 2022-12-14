b153edd8d8c65688d48c8fdbe90a347163d104d7
imam hossain <ihjony23@gmail.com>
2022-12-14 18:06:19 +0600

    875. Koko Eating Bananas

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val piles = intArrayOf(312884470)
+    val h = 312884469
+    println(
+        Solution().minEatingSpeed(piles, h)
+    )
+}
---
+++ src/main/kotlin/koko_eating_bananas.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun minEatingSpeed(piles: IntArray, h: Int): Int {
+        var min = 1
+        var max = piles.max() ?: 0
+        while (min < max) {
+            val mid = (max + min) / 2
+            val sumHour = tryEating(piles, h, mid)
+            if (sumHour > h) {
+                min = mid + 1
+            } else {
+                max = mid
+            }
+        }
+        return max
+    }
+
+    private fun tryEating(piles: IntArray, h: Int, k: Int): Int {
+        var hour = 0
+        for (pile in piles) {
+            hour += pile / k
+            hour += (if ((pile % k) != 0) 1 else 0)
+        }
+        return hour
+    }
+}
