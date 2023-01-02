d618ca3df11c9f5026910cf3b3641699a2d11e95
imam hossain <ihjony23@gmail.com>
2023-01-02 14:55:19 +0600

    3. Longest Substring Without Repeating Characters (sliding window)

--- src/main/kotlin/longest_substring_without_repeating_characters/Solution.kt
+++ src/main/kotlin/longest_substring_without_repeating_characters/Solution.kt
@@ -3,20 +3,32 @@ package longest_substring_without_repeating_characters
 class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
         var maxLength = 0
+        val hashMap: LinkedHashMap<Char, Int?> = LinkedHashMap()
+        var count = 0
         s.forEachIndexed { index, c ->
-            val hashMap: LinkedHashMap<Char, Boolean> = LinkedHashMap()
-            var iterationCount = 1
-            for (i in (index + 1) until s.length) {
-                if (s[i] == c || hashMap[s[i]] == true) {
-                    break
+            if (hashMap[c] == null) {
+                hashMap[c] = index
+                count++
+            } else {
+                maxLength = max(count, maxLength)
+                count = index - hashMap[c]!!
+                run loop@{
+                    mutableSetOf<Char>().apply {
+                        addAll(hashMap.keys)
+                    }.forEach {
+                        hashMap.remove(it)
+                        if (it == c) {
+                            return@loop
+                        }
+                    }
                 }
-                hashMap[s[i]] = true
-                iterationCount++
-            }
-            if (iterationCount > maxLength) {
-                maxLength = iterationCount
+                hashMap[c] = index
             }
         }
-        return maxLength
+        return max(count, maxLength)
+    }
+
+    private fun max(a: Int, b: Int): Int {
+        return if (a >= b) a else b
     }
 }
